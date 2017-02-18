67dd7f684e8c5b8780f737c0194c9ac0798dd958
kirito96 <eagle1968@live.com>
2017-02-18 11:34:19 -0400

    Initial commit

---
+++ yaris_m_gsm-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/joy_tv/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/joy_tv/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/joy_tv/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/joy_tv/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/joy_tv/proprietary/vendor,system/vendor)
